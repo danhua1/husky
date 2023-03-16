eff487c2c0d6c4f6e1c2884578c8c2078c36fcee
2485069913@qq.com <dusiyuan@dusiyuandeMacBook-Pro.local>
2023-03-16 11:06:06 +0800

    feat: 新项目

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteEslint from 'vite-plugin-eslint'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteEslint({
+      failOnError: false
+    })
+  ]
+})
